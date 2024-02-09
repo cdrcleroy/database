0ccabf722800ef9980a784d4754c9cc6836f2103
cedric <151522602+cdrcleroy@users.noreply.github.com>
2024-02-09 21:20:40 +0100

    Delete modification.sql

--- modification.sql
+++
@@ -1,7 +0,0 @@
-USE creParc;
-UPDATE Segment SET etage = 0 WHERE indIp = '130.120.80';
-UPDATE Segment SET etage = 1 WHERE indIp = '130.120.81';
-UPDATE Segment SET etage = 2 WHERE indIp = '130.120.82';
-UPDATE Logiciel SET prix = prix * 0.9 WHERE typeLog = 'PCNT';
-SELECT * FROM Segment;
-SELECT nLog, typeLog, prix FROM Logiciel;
